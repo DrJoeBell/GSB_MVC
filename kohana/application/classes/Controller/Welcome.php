e8b6bd472d2724eb8b5ff2b096cf605dafe06c04
Clement RICHARD <clement.richard443@gmail.com>
2014-04-11 11:17:38 +0200

    ajout index liste pages

--- kohana/application/classes/Controller/Welcome.php
+++ kohana/application/classes/Controller/Welcome.php
@@ -4,6 +4,60 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
-		$this->response->body('hello, world!');
+
+		$text=
+		'
+		<a href="'.url::base().'index.php/login">login</a>
+		<a href="'.url::base().'index.php/logout">logout</a>
+		<a href="'.url::base().'index.php/monCompte">Mon compte</a>
+		<a href="'.url::base().'index.php/">Modifier mon compte</a>
+		<ul>
+			<li>
+				<a href="'.url::base().'index.php/">Medicament</a>
+			</li>
+			<li>
+				<a href="'.url::base().'index.php/">Ajouter</a>
+			</li>
+			<li>
+				<a href="'.url::base().'index.php/">Supprimer</a>
+			</li>
+			<li>
+				<a href="'.url::base().'index.php/">Modifier</a>
+			</li>
+		</ul>
+		<ul>
+			<li>
+				<a href="'.url::base().'index.php/">Compte rendu</a>
+			</li>
+			<li>
+				<a href="'.url::base().'index.php/">Ajouter</a>
+			</li>
+			<li>
+				<a href="'.url::base().'index.php/">Supprimer</a>
+			</li>
+			<li>
+				<a href="'.url::base().'index.php/">Modifier</a>
+			</li>
+		</ul>
+		<ul>
+			<li>
+				<a href="'.url::base().'index.php/">Médecin</a>
+			</li>
+			<li>
+				<a href="'.url::base().'index.php/">Ajouter</a>
+			</li>
+			<li>
+				<a href="'.url::base().'index.php/">Supprimer</a>
+			</li>
+			<li>
+				<a href="'.url::base().'index.php/">Modifier</a>
+			</li>
+		</ul>
+
+
+		';
+
+
+		$this->response->body($text);
 	}
 } // End Welcome
