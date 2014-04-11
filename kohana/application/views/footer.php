96fd7317384c58d94a3aaad5286042b3dd047c20
Valentin Plissonneau <valentin.plissonneau@gmail.com>
2014-04-11 13:51:23 +0200

    ajout de la BDD

--- kohana/application/views/footer.php
+++ kohana/application/views/footer.php
@@ -10,6 +10,6 @@
 
 
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="<?= url::base(); ?>application/js/bootswatch.js"></script>
+    <script src="<?= url::base(); ?>application/js/bootstrap.js"></script>
   </body>
 </html>
